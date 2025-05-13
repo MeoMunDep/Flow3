8d09e679debf70b8d03b6ce1226ec7a382998203
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-14 01:28:26 +0700

    Update run.sh

--- flow3/run.sh
+++ flow3/run.sh
@@ -19,7 +19,16 @@ if [ ! -f "tokens.txt" ]; then
 fi
 
 echo "Setting environment variables..."
-
+[ -f .env ] || {
+  cat <<EOL > .env
+MAX_THREADS=10
+SLEEP_TIME=2
+DEFAULT_WALLETS=100
+TIMEOUT=10
+SKIP_ERROR_PROXIES=False
+EOL
+  echo ".env created"
+}
 
 echo "Starting Flow3 automation tool..."
 python3 meomundep.py
